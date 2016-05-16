23d31f483bd36c0e6b596d11784c18d1db48a3cc
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-05-16 18:38:11 -0400

    updated logic with warning for undefined search field

--- js/searchController.js
+++ js/searchController.js
@@ -4,16 +4,23 @@ searchApp.controller('searchController', function($scope, $http){
 	var apiUrl = 'https://api.github.com/search/users?q=';
 
 	$scope.searchUser = function(form){
-		$http.get(apiUrl + $scope.search, {})
-			.then(function successCallback(response){
-				console.log(response.data.items);
-				$scope.items = response.data.items;
+		if($scope.search != undefined){
+			$http.get(apiUrl + $scope.search, {})
+				.then(function successCallback(response){
+					if(response.data.items<=0){
+						$scope.errorMessage = "Please try a different user.  Your search returned 0 results.";
+					}
+					console.log(response.data.items);
+					$scope.items = response.data.items;
 
 
-			}, function errorCallback(response){
-				console.log(response);
-			});
+				}, function errorCallback(response){
+					console.log(response);
+				});
+		}else{
+			$scope.errorMessage = "Please enter a username."
 			
+		}
 	}
 
 
